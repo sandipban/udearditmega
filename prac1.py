4e1e80422926fce6d3fa96eae6de176317aecdf8
sandipban <sandipban@gmail.com>
2021-06-14 14:39:10 +0530

    1406211438

--- prac1.py
+++ prac1.py
@@ -1,14 +1,35 @@
-student_grade = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
-grade_byname = {'ram': 8.8, 'shyam': 8.8, 'jadu': 8.8}
-number_sum = sum(student_grade)
-number_of_items = len(student_grade)
-average = number_sum / number_of_items
-print(average)
-print(max(student_grade))
-print(student_grade.count(10.0))
-username = 'sandip1'
-print (username.title())
-byname_values = grade_byname.values()
-avg = sum(byname_values)/ len(byname_values)
-print(avg)
-print (grade_byname.values(), grade_byname.keys())
+# student_grade = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+# grade_byname = {'ram': 8.8, 'shyam': 8.8, 'jadu': 8.8}
+# number_sum = sum(student_grade)
+# number_of_items = len(student_grade)
+# average = number_sum / number_of_items
+# print(average)
+# print(max(student_grade))
+# print(student_grade.count(10.0))
+# username = 'sandip1'
+# print (username.title())
+# byname_values = grade_byname.values()
+# avg = sum(byname_values)/ len(byname_values)
+# print(avg)
+# print (grade_byname.values(), grade_byname.keys())
+
+# usr_input = input('Enter your name : ')
+# def foo(name):
+#     return(f'Hello, {name}')
+
+# print(foo(usr_input.title()))
+
+
+# colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
+# for num in colors:
+#     if isinstance(num, int):
+#         if num > 50 :
+#             print(num)
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"} #Looping through the dict.
+
+for key, value in phone_numbers.items():
+    print(f'{key} has the phone number : { value}')
+
+for num in phone_numbers.values():
+    print(f'00{num[1:]}')
